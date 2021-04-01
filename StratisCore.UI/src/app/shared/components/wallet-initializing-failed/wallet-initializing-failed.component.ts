9937c2efc1424a4954e4fd6f31d6625af27ac674
dev0tion <pieterjan.vanhoof@stratisplatform.com>
2021-04-01 16:43:04 +0200

    Separate initialization logos

---
+++ StratisCore.UI/src/app/shared/components/wallet-initializing-failed/wallet-initializing-failed.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-wallet-initializing-failed',
+  templateUrl: './wallet-initializing-failed.component.html',
+  styleUrls: ['./wallet-initializing-failed.component.scss']
+})
+export class WalletInitializingFailedComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ StratisCore.UI/src/app/shared/components/wallet-initializing/wallet-initializing.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-wallet-initializing',
+  templateUrl: './wallet-initializing.component.html',
+  styleUrls: ['./wallet-initializing.component.scss']
+})
+export class WalletInitializingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
