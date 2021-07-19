fd9ef871891955fb0e64d9f9f06d127057daaae3
Iain McCain <iain.mccain@stratisplatform.com>
2021-07-19 11:11:59 +0100

    bump to 1.3.2

--- src/app/shared/services/global.service.ts
+++ src/app/shared/services/global.service.ts
@@ -16,7 +16,7 @@ export class GlobalService {
     this.setDaemonIP();
   }
 
-  private applicationVersion = '1.3.1';
+  private applicationVersion = '1.3.2';
   private gitCommit = "";
   private testnet = false;
   private mainApiPort = 17103;
